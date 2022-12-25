367a9a06a0c4c6883c478223075d39315c4b6118
codjoelmayer <tmjoelm4@gmail.com>
2022-12-25 22:07:46 +0200

    Vue Examples

---
+++ two-way-binding/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>Two way binding</h2>
+  <DataBindingComp/>
+</template>
+
+<script>
+
+import DataBindingComp from './components/DataBindingComp.vue';
+export default {
+  name: 'App',
+  components: {
+    DataBindingComp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ two-way-binding/src/components/DataBindingComp.vue
@@ -0,0 +1,21 @@
+<template>
+    <form >
+        <div>
+            <input type="text" id="entry-data" placeholder="Enter your data" v-model="entryData" required>
+        </div>
+        <div>
+            <p >Entry data is: {{ entryData }}</p>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                entryData: null
+            }
+        },
+        
+    }
+</script>
